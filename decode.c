1d8f6b935d73619d78a877de7d32411195b002bb
Neverleft-code <56881079+Neverleft-code@users.noreply.github.com>
2019-11-16 00:43:30 +0800

    decoder.c

---
+++ decode.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int i;
+        int p[7];
+        int l[7];
+        for(i=0; i<7; i++)
+                s[i] = p[i]&1;
+        //HxL=z
