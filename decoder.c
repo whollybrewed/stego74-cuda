d04696d0e1c6470d1cac440383da50b06df3b90d
Neverleft-code <56881079+Neverleft-code@users.noreply.github.com>
2019-11-21 20:13:02 +0800

    Update decoder.c

--- decoder.c
+++ decoder.c
@@ -1,24 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-
+#include "grouping.h"
+  
 int main()
 {
         int i;
-        int p[7];
-        int l[7];
-        int s[7];
-        int H[3][7];
-        int z[3] = {0};
-        H[0] = {1,0,1,0,1,0,1};
-        H[1] = {0,1,1,0,0,1,1};
-        H[2] = {0,0,0,1,1,1,1};
+        int p[7]={14,14,13,13,14,15,15};  //pixels
+        int l[7];  //LSB
+        int s[7];  //secret bits
+        int z[3] = {0};  //syndrome
         for(i=0; i<7; i++)
                 l[i] = p[i]&1;
         //HxL=z
         for(i=0; i<7; i++){
-                z[0] = z[0]^(H[0][i]*l[i]);
-                z[1] = z[1]^(H[1][i]*l[i]);
-                z[2] = z[2]^(H[2][i]*l[i]);
+                z[0] = z[0]^(matrix_h[0][i]*l[i]);
+                z[1] = z[1]^(matrix_h[1][i]*l[i]);
+                z[2] = z[2]^(matrix_h[2][i]*l[i]);
         }
         s[0] = z[0];
         s[1] = z[1];
