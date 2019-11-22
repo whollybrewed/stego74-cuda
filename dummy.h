663398c0754a9aa2732e3e01706edca84dd6736a
whollybrewed <andylee47695@gmail.com>
2019-11-22 23:08:31 +0800

    testing data

---
+++ dummy.h
@@ -0,0 +1,28 @@
+/*
+ *========================================================
+ * this header contains dummy data for debug purpose only
+ *========================================================
+ */
+
+#ifndef DUMMY_H
+#define DUMMY_H
+
+#include <stdlib.h>
+
+#define width 1024
+#define height 1024
+
+unsigned char data[25] = {
+    255, 255, 255, 255, 255,
+    254, 254, 254, 254, 254,
+    255, 255, 255, 255, 255,
+    254, 254, 254, 254, 254
+};
+unsigned char secrets [24] = {
+    1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0,
+    1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 0, 0
+};
+
+#endif
