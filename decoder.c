ea3b6d3123354770d840414a4f1604fc5e83276b
Kevin <kk2452080486@gmail.com>
2019-12-08 17:13:02 +0800

    modify indent and add BitsToString

--- decoder.c
+++ decoder.c
@@ -1,56 +1,68 @@
-#include "grouping.h"
+#include "decoder.h"
   
-void decode(unsigned char *p)
+void decode(unsigned char *p, const int height, const int width)
 {
-        int i,j;
-        const int num_secret = height * width - 1;
-        const int num_group = (height * width - 1)/7;
-        const int remain = (height * width - 1) % 7;
-        //int p[7]={14,14,13,13,14,15,15};  //pixels
-        unsigned char l[num_secret-remain+7];  //LSB
-        unsigned char s[num_secret-remain+7];  //secret bits
-        unsigned char z[num_group*3+3];  //syndrome
-		// initialize z to 0
-		for(i=0; i<num_group*3+3; i++)
-			z[i] = 0;
-        
-        for(i=0; i<num_secret-remain; i++)
-                l[i] = p[i]&1;
-        //HxL=z
-        for(j=0; j<num_group; j++){
-                for(i=0; i<7; i++){
-                        z[j*3] = z[j*3]^(matrix_h[0][i]*l[j*7+i]);
-                        z[j*3+1] = z[j*3+1]^(matrix_h[1][i]*l[j*7+i]);
-                        z[j*3+2] = z[j*3+2]^(matrix_h[2][i]*l[j*7+i]);
-                }
+    int i,j;
+    const int num_secret = height * width - 1;
+    const int num_group = (height * width - 1)/7;
+    const int remain = (height * width - 1) % 7;
+    //int p[7]={14,14,13,13,14,15,15};  //pixels
+    unsigned char l[num_secret-remain+7];  //LSB
+    unsigned char s[num_secret-remain+7];  //secret bits
+    unsigned char z[num_group*3+3];  //syndrome
+	// initialize z to 0
+	for(i=0; i<num_group*3+3; i++)
+		z[i] = 0;
+    
+    for(i=0; i<num_secret-remain; i++)
+        l[i] = p[i]&1;
+    //HxL=z
+    for(j=0; j<num_group; j++){
+        for(i=0; i<7; i++){
+            z[j*3] = z[j*3]^(matrix_h[0][i]*l[j*7+i]);
+            z[j*3+1] = z[j*3+1]^(matrix_h[1][i]*l[j*7+i]);
+            z[j*3+2] = z[j*3+2]^(matrix_h[2][i]*l[j*7+i]);
         }
-        for(j=0; j<num_group; j++){
-                s[j*7] = z[j*3];
-                s[j*7+1] = z[j*3+1];
-                s[j*7+2] = l[j*7+2];
-                s[j*7+3] = z[j*3+2];
-                s[j*7+4] = l[j*7+4];
-                s[j*7+5] = l[j*7+5];
-                s[j*7+6] = l[j*7+6];
+    }
+    for(j=0; j<num_group; j++){
+        s[j*7] = z[j*3];
+        s[j*7+1] = z[j*3+1];
+        s[j*7+2] = l[j*7+2];
+        s[j*7+3] = z[j*3+2];
+        s[j*7+4] = l[j*7+4];
+        s[j*7+5] = l[j*7+5];
+        s[j*7+6] = l[j*7+6];
+    }
+    //deal with the remainder
+    if(remain>0){
+        for(j=num_secret-remain+6; j>=num_secret-remain; j--){
+            l[j] = p[num_secret-1]&1;
+            p[num_secret-1] = p[num_secret-1]>>1;
         }
-        //deal with the remainder
-        if(remain>0){
-                for(j=num_secret-remain+6; j>=num_secret-remain; j--){
-                        l[j] = p[num_secret-1]&1;
-                        p[num_secret-1] = p[num_secret-1]>>1;
-                }
-                for(i=0; i<7; i++){
-                        z[num_group*3] = z[num_group*3]^(matrix_h[0][i]*l[num_secret-remain+i]);
-                        z[num_group*3+1] = z[num_group*3+1]^(matrix_h[1][i]*l[num_secret-remain+i]);
-                        z[num_group*3+2] = z[num_group*3+2]^(matrix_h[2][i]*l[num_secret-remain+i]);
-                }
-                s[num_group*7] = z[num_group*3];
-                s[num_group*7+1] = z[num_group*3+1];
-                s[num_group*7+2] = l[num_group*7+2];
-                s[num_group*7+3] = z[num_group*3+2];
-                s[num_group*7+4] = l[num_group*7+4];
-                s[num_group*7+5] = l[num_group*7+5];
-                s[num_group*7+6] = l[num_group*7+6];
+        for(i=0; i<7; i++){
+            z[num_group*3] = z[num_group*3]^(matrix_h[0][i]*l[num_secret-remain+i]);
+            z[num_group*3+1] = z[num_group*3+1]^(matrix_h[1][i]*l[num_secret-remain+i]);
+            z[num_group*3+2] = z[num_group*3+2]^(matrix_h[2][i]*l[num_secret-remain+i]);
         }
-        return;
+        s[num_group*7] = z[num_group*3];
+        s[num_group*7+1] = z[num_group*3+1];
+        s[num_group*7+2] = l[num_group*7+2];
+        s[num_group*7+3] = z[num_group*3+2];
+        s[num_group*7+4] = l[num_group*7+4];
+        s[num_group*7+5] = l[num_group*7+5];
+        s[num_group*7+6] = l[num_group*7+6];
+    }
+    
+    return;
+}
+
+void BitsToString(char* bits, int bits_size, char* string)
+{
+    for(int n=0; n<bits_size; n+=8) {
+        string[n] = 0;
+        for (int j = 0; j < 8; j++){
+            string[n] += bits[8*n+j];
+            string[n] = string[n] << 1;
+        }
+    }
 }
---
+++ decoder.h
@@ -0,0 +1,4 @@
+#include "grouping.h"
+
+void decode(unsigned char *p, const int height, const int width);
+void BitsToString(char* bits, int bits_size, char* string);
