1f4cf19d06cc6451b0aa1d35a8244967613bbead
Kevin <kk2452080486@gmail.com>
2019-12-21 15:28:58 +0800

    finish output file to txt

--- main.c
+++ main.c
@@ -30,6 +30,9 @@ int main(int argc, char* argv[])
     char* message = (char*)malloc((secret_size/8)*sizeof(char)+1);
     decode(decoder.data, secret_size, message);
     printf("message = %s\n", message);
+
+    // output txt file
+    OutputTxt("message.txt", message);
     free(message);
     return 0;
 }
